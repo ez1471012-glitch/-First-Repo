38d6e6021ef79eced55c62a74f6c7656f4e1a03c
Eman Zahra <ez1471012@gmail.com>
2025-12-28 19:46:26 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[,] names = new string[,]
+            {
+                { "Ali", "Hassan" },
+                { "Arooj", "Arbab" },
+                { "Amna", "Areej" },
+                { "Noor", "Zaib" }
+            };
+
+           
+            for (int i = 0; i <= names.GetUpperBound(0); i++)
+            {
+                string firstName = names[i, 0];
+                string lastName = names[i, 1];
+
+                Console.WriteLine("{0} {1}", firstName, lastName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
